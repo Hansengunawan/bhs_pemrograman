3989e0151335e2aa863ba823c042cc83088647da
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-10-06 18:36:19 +0700

    tugas 3

--- pert3/tugas3.py
+++ pert3/tugas3.py
@@ -28,8 +28,20 @@ print("Pembagian")
 total=nilai1/nilai2 
 print("nilai pembagian", total)
 
+#Modulus/nilai sisa
+print("Modulus")
+total=nilai1%nilai2
+print("nilai modulus", total)
 
+#exponentiation/pangkat
+print("Exponentiation")
+total=nilai1**nilai2
+print("nilai exponentiation", total)
 
+#floordivision/nilai terdekat
+print("Floordivision")
+total=nilai1//nilai2
+print("nilai terdekat", total)
 
 
 
