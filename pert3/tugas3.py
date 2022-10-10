b29791197262009721e3086a3eeeffae85b536e4
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-10-10 19:20:30 +0700

    fresh from oven

--- pert3/tugas3.py
+++ pert3/tugas3.py
@@ -1,47 +1,92 @@
-#Operator pada Pemrograman Python
+#Operator Aritmatika pada Pemrograman Python
 
-a="Hansen\n"
-b=20210801355
+a="Hansen"
+b="\n20210801355"
 c="\nSample Operator in Pyhton"
+d="\nOperator Aritmatika in Python"
 
-print (a,b,c)
+print (a,b,c,d)
 
-#Penjumlahan
-print ("Penjumlahan")
+#Penjumlahan (+)
+print ("Penjumlahan (+)")
 nilai1=int(input("nilai 1 = "))
 nilai2=int(input("nilai 2 = "))
 total=nilai1+nilai2
 print("nilai penjumlahan", total)
 
 #Pengurangan
-print("Pengurangan")
+print("Pengurangan (-) ")
 total=nilai1-nilai2
 print("nilai pengurangan", total)
 
 #Perkalian
-print("Perkalian")
+print("Perkalian (*) ")
 total=nilai1*nilai2
 print("nilai perkalian", total)
 
 #Pembagian
-print("Pembagian")
+print("Pembagian (/)")
 total=nilai1/nilai2 
 print("nilai pembagian", total)
 
 #Modulus/nilai sisa
-print("Modulus")
+print("Modulus (%) ")
 total=nilai1%nilai2
 print("nilai modulus", total)
 
 #exponentiation/pangkat
-print("Exponentiation")
+print("Exponentiation (**) ")
 total=nilai1**nilai2
 print("nilai exponentiation", total)
 
 #floordivision/nilai terdekat
-print("Floordivision")
+print("Floordivision (//) ")
 total=nilai1//nilai2
 print("nilai terdekat", total)
 
-#
+
+#Operator Penugasan pada Pemrograman Python
+
+print("Operator Penugasan in Python")
+
+#Pengisian Nilai
+print("Pengisian Nilai (=)")
+x1=int(input("x1 = "))
+print(x1)
+
+#Pengisian dan Penambahan
+print("Pengisian dan penambahan (+=) ")
+x1 +=8
+print(x1)
+
+#Pengisian dan Pengurangan
+print("Pengisian dan Pengurangan (-=)")
+x1-= 2
+print(x1)
+
+#Pengisian dan Perkalian
+print("Pengisian dan Perkalian (*=) ")
+x1*= 2
+print(x1)
+
+#Pengisian dan Pembagian 
+print("Pengisian dan Pembagian (/=) ")
+x1/=2
+print(x1)
+
+#Pengisian dan Modulus
+print("Pengisian dan Modulus (%=)")
+x1%=3
+print(x1)
+
+#Pengisian dan FloorDivision
+print("Pengisian dan FloorDivision (//=)")
+x1//=2
+print(x1)
+
+#Pengisian dan Exponentiation
+print("Pengisian dan Exponentiation (**=) ")
+x1**=3
+print(x1)
+
 
