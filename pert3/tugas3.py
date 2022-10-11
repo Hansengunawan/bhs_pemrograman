c68458f0ae893ba43f2c1d7c105ec3ba68e26ee4
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-10-11 19:53:57 +0700

    fresh from oven

--- pert3/tugas3.py
+++ pert3/tugas3.py
@@ -8,6 +8,7 @@ d="\nOperator Aritmatika in Python"
 print (a,b,c,d)
 
 #Penjumlahan (+)
+print("----------------")
 print ("Penjumlahan (+)")
 nilai1=int(input("nilai 1 = "))
 nilai2=int(input("nilai 2 = "))
@@ -46,7 +47,7 @@ print("nilai terdekat", total)
 
 
 #Operator Penugasan pada Pemrograman Python
-
+print("----------------")
 print("Operator Penugasan in Python")
 
 #Pengisian Nilai
@@ -119,4 +120,111 @@ print(y)
 print("Pengisian dan XOR (^=)")
 y^=4
 print(y)
+
+#Pengisian dan Right Shift
+print("Pengisian dan Right Shift (>>=) ")
+y>>=2
+print(y)
+
+#Pengisian dan Left Shift
+print("Pengisian dan Left Shift (<<=)")
+y<<=3
+print(y)
+
+print("----------------")
+
+#Operator Perbandingan In Python
+print("Operator Perbandingan In Python")
+
+#Equal/Setara
+print("Equal (==)")
+y2 = 2
+print (y==y2)
+
+#Not Equal/Tidak Setara
+print("Not Equal (!=)")
+print (y!=y2)
+
+#Greater than/Lebih besar dari
+Nilai1=4
+Nilai2=2
+
+print("Greater than (>)")
+print("Nilai 1 = ", Nilai1)
+print("Nilai 2 = ", Nilai2)
+print(Nilai1>Nilai2)
+
+#Less than/Lebih kecil dari
+print("Less than (<)")
+print(Nilai1<Nilai2)
+
+#Greater than or equal to
+print("Greater than or equal to (<=)")
+print(Nilai1>=Nilai2)
+
+#Less than or equal to
+print("Less than or equal to (<=)")
+print(Nilai1<=Nilai2)
+
+print("----------------")
+
+#Operator Logika in Python
+print("Operator Logika in python")
+
+#and (returns true if both statements are true)
+print("and (returns true if both statements are true)")
+print(Nilai1>3 and Nilai1<5)
+
+#or (returns true if one of the statements is true)
+print("or (returns true if one of the statements is true)")
+print(Nilai1>3 or Nilai2<5)
+
+#not (reverse the result, returns False if the result is true)
+print("not (reverse the result, returns False if the result is true)")
+print(not (Nilai1>3 and Nilai2<5))
+
+print("----------------")
+
+#Operator Identitas in Python
+print("Operator Identitas in Python")
+
+#is Returns True if both variables are the same object
+print("is (Returns True if both variables are the same object)")
+k = ["ketoprak, gado-gado"]
+l = ["ketoprak, gado-gado"]
+j = k
+
+print(k)
+print(l)
+
+print(k is j)
+#returns True because j is the same object as k
+
+print(k is l)
+#returns False because k is not the same object as l, even if they have the same content
+
+print(k==l)
+#to demonstrate the difference betweeen "is" and "==": this comparison returns True because k is equal to ;
+
+print("----------------")
+
+#Operator Membership in Python
+print("Operator Membership in Python")
+
+#in Returns True if a sequence with the specified value is present in the object	
+print("(in Returns True if a sequence with the specified value is present in the object)")
+
+print("gado-gado," in k)
+#returns True because a sequence with the value "gado-gado" is in the list
+
+print("karedok" in k)
+#returns False because a sequence with the value "karedok" not in the list
+
+print("----------------")
+
+#not in Returns True if a sequence with the specified value is not present in the object
+print("not in (Returns True if a sequence with the specified value is not present in the object)")
+
+print("karedok" not in k)
+# returns True because a sequence with the value "karedok" is not in the list
 
