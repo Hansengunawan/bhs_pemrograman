6d49586ff657f3473dea021d6de3188eb5a3a3d8
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-10-12 21:19:50 +0700

    fresh from oven

---
+++ pert4/variable.py
@@ -0,0 +1,24 @@
+#Variable
+
+a, b =5, 10
+penjumlahan = a+b
+print("Hasil dari a+b = ", penjumlahan)
+
+def penjumlahan ():
+
+    a=int(input("masukkan angka pertama : "))
+    b=int(input("masukkan angka kedua : "))
+    c=a+b
+    print(c)
+
+while True:
+    print("1. Menu")
+    print("2. Penjumlahan")
+    print("3. Exit")
+    choice = int(input("pilihanmu :"))
+    if choice == 1 :
+        penjumlahan()
+    elif choice == 2:
+        break
+    else:
+        print("pilihan salah")
