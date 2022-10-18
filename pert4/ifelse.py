9293c0d31211816d35b002577ec683d6653ca45c
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-10-18 16:18:17 +0700

    fresh from oven

--- pert4/ifelse.py
+++ pert4/ifelse.py
@@ -6,12 +6,59 @@ def login():
     print("="*100)
     user=input("Username : ")
     pw=input("Password : ")
-    
+
     if user == "Hansen" and pw == "hansen":
         print("Login Berhasil")
+        AKADEMIK()
     else:
-        print("Coba Lagi")
-login()
+        login_salah() 
     
+def login_salah():
+    print("Silahkan Pilih Kembali")
+    login()
+
+def AKADEMIK() :
+    print("1. Data")
+    print("2. Perkuliahan")
+    print("3. Registrasi Semester")
+    print("4. Daftar Nilai")
+    x = input("=>> : ")
+
+    if x == "1" : 
+        data()
+    elif x == "2" :
+        Perkuliahan()
+    elif x == "3" :
+        Registrasi_Semester()
+    elif x == "4" :
+        Daftar_Nilai()
+    else :
+        print("Coba Lagi")
     
 
+def data(): 
+    print("1. Biodata Mahasiswa")
+    print("2. Tagihan Mahasiswa")
+    print("3. Surat Keterangan Lulus")
+   
+
+def Perkuliahan():
+    print("1. Kurikulum")
+    print("2. Kegiatan Akademik")
+   
+
+def Registrasi_Semester():
+    print("1. KRS")
+    print("2. Jadwal Kuliah")
+    print("3. Absensi Kuliah")
+    print("4. Quisioner")
+   
+
+def Daftar_Nilai():
+    print("1. KHS")
+    print("2. Kemajuan Belajar")
+    print("3. Riwayat Mengulang")
+    print("4. Resume Nilai")
+
+    
+login()
