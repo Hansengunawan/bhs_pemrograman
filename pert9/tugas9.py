216d8ac3eac2d6e6dc03c71e4977a13b615fdf69
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2022-12-05 17:08:28 +0700

    fresh from oven

---
+++ pert9/tugas9.py
@@ -0,0 +1,14 @@
+print ("Pointer in Python")
+
+x = 20
+y = 10
+print("x : ",x)
+print("Address x :",id(x))
+
+print("y : ",y)
+print("Address y :",id(y))
+
+print("")
+z=input("Angka yang ingin dialamatkan :  ")
+print("Alamatnya adalah : " ,id(z))
+
