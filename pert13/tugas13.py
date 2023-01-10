cb53f28a142e90631e667f84025562f836d5b933
Hansengunawan <Hansengunawan264@student.esaunggul.ac.id>
2023-01-10 21:10:28 +0700

    fresh from oven

---
+++ pert13/tugas13.py
@@ -0,0 +1,13 @@
+# biodata
+
+class DATA:
+    def __init__(self,Nama,Jurusan,TglLahir):
+        self.nama=Nama
+        self.jurusan=Jurusan
+        self.tglLahir=TglLahir
+
+a = DATA ("Hansen","Teknik Informatika","07-07-2003")
+print("Nama         = ",a.nama)
+print("Jurusan      = ",a.jurusan)
+print("TglLahir     = ",a.tglLahir)
+    
